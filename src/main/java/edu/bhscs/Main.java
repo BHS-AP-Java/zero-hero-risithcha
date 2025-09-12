9093571786d3e54b05eb7bfd9c4e3cb776014daf
Risith Kankanamge <83322661+risithcha@users.noreply.github.com>
2025-09-12 16:46:46 +0000

    I am mastering PRINTLN

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,23 @@
-// Student name
-// class period
-// project name
-// date
+// Risith Kankanamge
+// P2
+// Zero-Hero
+// 09/12/2025
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber says
+ * INPUT: We don't know about input yet
+ * OUTPUT: Some cool works in the command line terminal thing
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("---");
+    System.out.println("Program Start");
+    System.out.println("Hello World");
+    System.out.println("Program End");
+    System.out.println("---");
+  }
+}
